861a391b369ff66f06fc5114c3e89f99402953cf
FILL16Z <61636344+FILL16Z@users.noreply.github.com>
2020-10-26 00:25:43 -0700

    Updates

---
+++ precache-manifest.d39a0ae024b6e6a380fbafcecb983578.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43fd746b5437422412f93f494d9b612",
+    "url": "/Practica1React/index.html"
+  },
+  {
+    "revision": "b873d5891c4907ed9f73",
+    "url": "/Practica1React/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c8edae75743fb91bbd72",
+    "url": "/Practica1React/static/css/main.cf3d632f.chunk.css"
+  },
+  {
+    "revision": "b873d5891c4907ed9f73",
+    "url": "/Practica1React/static/js/2.19ed1727.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Practica1React/static/js/2.19ed1727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8edae75743fb91bbd72",
+    "url": "/Practica1React/static/js/main.09ae27b9.chunk.js"
+  },
+  {
+    "revision": "ab93fe5fd32e3195a9dc",
+    "url": "/Practica1React/static/js/runtime-main.cba388a1.js"
+  },
+  {
+    "revision": "96aff77c5833d91511a87f051dc63231",
+    "url": "/Practica1React/static/media/logotec.96aff77c.jpg"
+  }
+]);
